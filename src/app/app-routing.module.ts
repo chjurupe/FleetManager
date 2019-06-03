995eed3abf48879bc8fbd110fb0ab5fe5df62935
Christian <christian.jurupe@gmail.com>
2019-06-03 12:59:47 -0500

    Routes Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { OrderComponent } from './order/order.component';
+import { TaskComponent } from './task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Vehicles', component: VehicleComponent },
+  { path: 'Orders', component: OrderComponent },
+  { path: 'Tasks', component: TaskComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
