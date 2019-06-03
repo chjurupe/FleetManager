06b09d44f8ae3661a3c4d123ec89c285ccc3dfdb
Christian <christian.jurupe@gmail.com>
2019-06-03 12:44:14 -0500

    Components and menu created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { OrderComponent } from './order/order.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VehicleComponent,
+    OrderComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
